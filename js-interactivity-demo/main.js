5586a10659996a126843043e093c2f9ed862c19d
Jonathan Ho <98234461+jho97@users.noreply.github.com>
2022-04-06 18:18:14 -0500

    demo

---
+++ js-interactivity-demo/main.js
@@ -0,0 +1,45 @@
+let count =  0
+const counterText = document.querySelector('#counter')
+
+const increase = () => {
+    count += 1
+    counterText.textContent = count
+}
+
+const decrease = () => {
+    count -= 1 
+    counterText.textContent = count
+
+}
+const reset = () => {
+    count = 0
+    counterText.textContent = count
+
+}
+const plusBtn = document.querySelector('#plus-btn')
+const minusBtn = document.querySelector('#minus-btn')
+const resetBtn = document.getElementById('reset-btn')
+
+plusBtn.addEventListener('click', increase)
+minusBtn.addEventListener('click', decrease)
+resetBtn.addEventListener('click', reset)
+
+const buttons = document.querySelectorAll('.theme-buttons')
+
+const selectTheme = (event) => {
+    const theme = event.target.textContent
+
+    const body = document.querySelector('body')
+    const main = document.querySelector('main')
+
+    body.className = theme
+    main.className = theme
+
+    for(let i = 0; i < buttons.length; i++) {
+        buttons[i].className = theme
+    }
+}
+
+for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', selectTheme)
+}
